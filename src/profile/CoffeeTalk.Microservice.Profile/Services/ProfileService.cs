36da5a23a2bd6b08ab2447d978c7e5ebd8d10e15
Westenbrink, Friso F <Proxyle@users.noreply.github.com>
2020-10-28 16:14:28 +0100

    Added Dockerfile

--- src/profile/CoffeeTalk.Microservice.Profile/Services/ProfileService.cs
+++ src/profile/CoffeeTalk.Microservice.Profile/Services/ProfileService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using CoffeeTalk.Microservice.Profile.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoffeeTalk.Microservice.Profile.Services
@@ -10,9 +11,9 @@ namespace CoffeeTalk.Microservice.Profile.Services
         {
         }
 
-        public async Task<IActionResult> GetProfileData() 
+        public Task<ProfileEntity> GetProfile(Guid profileId)
         {
-            
+            throw new NotImplementedException();
         }
     }
 }
