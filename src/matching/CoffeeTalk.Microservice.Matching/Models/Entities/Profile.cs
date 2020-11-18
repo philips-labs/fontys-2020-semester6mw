040e3d028d931454810a4ea5d7bb57d3a127d46f
Westenbrink, Friso F <Proxyle@users.noreply.github.com>
2020-11-18 16:27:55 +0100

    Finished algoritm, not properly tested yet

--- src/matching/CoffeeTalk.Microservice.Matching/Models/Entities/Profile.cs
+++ src/matching/CoffeeTalk.Microservice.Matching/Models/Entities/Profile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace CoffeeTalk.Microservice.Matching.Models.Entities
--- src/matching/CoffeeTalk.Microservice.Matching/Service/Matcher.cs
+++ src/matching/CoffeeTalk.Microservice.Matching/Service/Matcher.cs
@@ -9,11 +9,21 @@ namespace CoffeeTalk.Microservice.Matching.Service
     {
         private List<Profile> profiles;
         private Dictionary<string, List<string>> existingMatches;
+        private Dictionary<string, List<string>> newPossibleMatches;
+
+        public Matcher(List<Profile> profiles, Dictionary<string, List<string>> existingMatches, Dictionary<string, List<string>> newPossibleMatches)
+        {
+            this.profiles = profiles;
+            this.existingMatches = existingMatches;
+            this.newPossibleMatches = newPossibleMatches;
+        }
 
         public void CreateMatches() 
         {
             foreach (var profile in profiles) 
             {
+                Dictionary<string, double> possibleMatchPercentDictionary = new Dictionary<string, double>();
+
                 for (int i = 0; i < profiles.Count; i++)
                 {
                     var possibleMatchProfile = profiles[i];
@@ -21,9 +31,36 @@ namespace CoffeeTalk.Microservice.Matching.Service
                     if (possibleMatchProfile.Equals(profile)) continue;
                     if (matchAlreadyExists(profile.Id, possibleMatchProfile.Id)) continue;
 
+                    double interestsPercentage = compareInterests(profile.Interests, possibleMatchProfile.Interests);
+                    double projectsPercentage = compareInterestsWithProjects(profile.Interests, possibleMatchProfile.PreviousProjects);
 
+                    double truePercentage = (interestsPercentage * 0.7) + (projectsPercentage * 0.3);
+
+                    possibleMatchPercentDictionary.Add(possibleMatchProfile.Id, truePercentage);
                 }
+
+                var possibleMatchPercentList = possibleMatchPercentDictionary.ToList();
+                possibleMatchPercentList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+                newPossibleMatches.Add(profile.Id, (from kvp in possibleMatchPercentList select kvp.Key).ToList());
             }
+        }
+
+        private double compareInterestsWithProjects(List<string> interestsA, List<Project> projectsB)
+        {
+            List<string> tags = new List<string>();
+            foreach (var project in projectsB)
+            {
+                project.Tags.ForEach(t => tags.Add(t));
+            }
+
+            return compareInterests(interestsA, tags);
+        }
+
+        private double compareInterests(List<string> interestsA, List<string> interestsB)
+        {
+            var commonInterests = interestsA.Intersect(interestsB);
+            return (commonInterests.Count() / interestsA.Count()) * 100;
         }
 
         private bool matchAlreadyExists(string profileId, string possibleMatchId)
